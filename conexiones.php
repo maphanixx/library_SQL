21aa1155d2aa18e8b87825ac3aedd9bb677f62fd
maphanixx <81342646+maphanixx@users.noreply.github.com>
2022-05-24 22:36:12 -0500

    Add files via upload

--- conexiones.php
+++ conexiones.php
@@ -1,3 +1,81 @@
+<<<<<<< HEAD
+<?php
+
+include_once ("config.php");
+class CRUD {
+
+ // actualizar los datos de los clientes
+    public function update(){
+
+        $con=conectar();
+
+            $cod_estudiante=$_POST['cod_estudiante'];
+            $cedula=$_POST['cedula'];
+            $nombres=$_POST['nombres'];
+            $apellidos=$_POST['apellidos'];
+
+            $sql="UPDATE alumno SET  cedula='$cedula',nombres='$nombres',apellidos='$apellidos' WHERE cod_estudiante='$cod_estudiante'";
+            $query=mysqli_query($con,$sql);
+
+            if($query){
+                Header("Location: alumno.php");
+            }
+
+    }
+
+    // borrar los datos de los clientes
+    public function delete(){
+
+        $con=conectar();
+
+            $cod_estudiante=$_GET['id'];
+
+            $sql="DELETE FROM alumno  WHERE cod_estudiante='$cod_estudiante'";
+            $query=mysqli_query($con,$sql);
+
+            if($query){
+                Header("Location: alumno.php");
+            }
+
+    }
+
+    // ingresar nuevos datos de los clientes 
+    public function insert(){
+
+        $con=conectar();
+
+            $cod_estudiante=$_POST['cod_estudiante'];
+            $cedula=$_POST['cedula'];
+            $nombres=$_POST['nombres'];
+            $apellidos=$_POST['apellidos'];
+
+
+            $sql="INSERT INTO alumno VALUES('$cod_estudiante','$cedula','$nombres','$apellidos')";
+            $query= mysqli_query($con,$sql);
+
+            if($query){
+                Header("Location: alumno.php");
+    
+            }else {
+        }
+
+    }
+
+    // mostrar los datos de los clientes en pantalla
+    public function select(){
+
+        $con=conectar();
+
+            $id=$_GET['id'];
+        
+            $sql="SELECT * FROM alumno WHERE cod_estudiante='$id'";
+            $query=mysqli_query($con,$sql);
+        
+            $row=mysqli_fetch_array($query);
+    }
+
+
+=======
 <?php
 
 class CRUD {
@@ -29,4 +107,5 @@ class CRUD {
     }
 
 
+>>>>>>> f575bc69225674fdac70c0f943ae44511c9a452f
 }
--- config.php
+++ config.php
@@ -1,3 +1,21 @@
+<<<<<<< HEAD
+<?php
+
+function conectar(){
+    $host="localhost";
+    $user="root";
+    $pass="";
+
+    $bd=" ";
+
+    $con=mysqli_connect($host,$user,$pass);
+
+    mysqli_select_db($con,$bd);
+
+    return $con;
+}
+=======
 <?php
 
 $mysqli = new mysqli('localhost', 'root', $pwd, 'database');
+>>>>>>> f575bc69225674fdac70c0f943ae44511c9a452f
